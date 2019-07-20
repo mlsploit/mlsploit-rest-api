9a351136e68b919b719ace4f36b09d57f6a925a7
Nilaksh Das <nilakshdas@gatech.edu>
2019-07-20 19:13:39 -0400

    Update endpoint for static files

--- api/settings.py
+++ api/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 
 REST_FRAMEWORK = {
